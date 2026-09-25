47628ef5c4c666ded09b213108c9fa7f981adaf4
eulo <sylowp9@gmail.com>
2026-09-25 14:53:51 -0600

    Added keyboard support

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ const clear = document.querySelector(".button-clear");
 const erase = document.querySelector(".button-erase");
 const decimal = document.querySelector(".button-decimal");
 
+const numberKeys = "0123456789";
+const operatorKeys = "/*+-";
+
 var firstNumber = "ERROR";
 var secondNumber = "ERROR";
 var currentOperator = "ERROR";
-var isResult = true; 
+var isResult = false; 
 
 function add(x,y){
     return Number(x) + Number(y);
@@ -45,41 +48,99 @@ function operate (x, y, op){
     }
 }
 
-
 numbers.forEach((number) => {
     number.addEventListener("click", () => {
-        if (isResult){
-            display.textContent = "";
-            isResult = false;
-        }
-        if (display.textContent.length < 17){
-            display.textContent += number.textContent;
-        }
+        appendDisplay(number.textContent);
     });
 });
-
 
 operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
+    operator.addEventListener("click", () => {
         // event.target.style.border = "solid 6px orange"; 
-        if (firstNumber === "ERROR" || currentOperator === "ERROR"){
-            currentOperator = operator.textContent; 
-            firstNumber = display.textContent;
-            display.textContent = "";
-        }
-        else {
-            var nextOperator = operator.textContent;
-            secondNumber = display.textContent;
-            var result = operate(firstNumber, secondNumber, currentOperator);
-            display.textContent = result;
-            firstNumber = result; 
-            currentOperator = nextOperator;
-            secondNumber = "ERROR";
-        }
+        setOperator(operator.textContent)
     });
 });
 
-equals.addEventListener("click", () => {
+equals.addEventListener("click", evaluate);
+
+clear.addEventListener("click", clearDisplay);
+
+erase.addEventListener("click", eraseDisplay);
+
+decimal.addEventListener("click", addDecimalToDisplay);
+
+document.addEventListener("keydown", (event) => {
+    if (numberKeys.includes(event.key) && display.textContent.length < 17){
+        appendDisplay(event.key);
+    }
+    else if (event.key === "Backspace"){
+        eraseDisplay();
+    }
+    else if (event.key === "Delete"){
+        clearDisplay();
+    }
+    else if (event.key === "."){
+        addDecimalToDisplay();
+    }
+    else if (operatorKeys.includes(event.key)){
+        setOperator(event.key);
+    }
+    else if (event.key === "Enter" || event.key === "="){
+        evaluate();
+    }
+});
+
+// Helper Functions 
+
+function clearDisplay(){
+    firstNumber = "ERROR";
+    secondNumber = "ERROR";
+    currentOperator = "ERROR";
+    isResult = false;
+    display.textContent = "";
+}
+
+function appendDisplay(number){
+    if (isResult){
+        display.textContent = "";
+        isResult = false;
+    }
+    if (display.textContent.length < 17){
+        display.textContent += number;
+    }
+}
+
+function eraseDisplay(){
+    var currentDisplay = display.textContent;
+    if (currentDisplay !== "" && !isResult){
+        display.textContent = currentDisplay.slice(0, currentDisplay.length-1);
+    }
+}
+
+function addDecimalToDisplay(){
+    if (!display.textContent.includes(".")){
+        display.textContent += ".";
+    }
+}
+
+function setOperator(operator){
+    if (firstNumber === "ERROR" || currentOperator === "ERROR"){
+        currentOperator = operator;
+        firstNumber = display.textContent;
+        display.textContent = "";
+    }
+    else {
+        var nextOperator = operator;
+        secondNumber = display.textContent;
+        var result = operate(firstNumber, secondNumber, currentOperator);
+        display.textContent = result;
+        firstNumber = result; 
+        currentOperator = nextOperator;
+        secondNumber = "ERROR";
+    }
+}
+
+function evaluate(){
     if (secondNumber === "ERROR" || display.textContent !== ""){
         secondNumber = display.textContent;
     }
@@ -90,26 +151,4 @@ equals.addEventListener("click", () => {
         secondNumber = "ERROR";
         currentOperator = "ERROR";
     }
-});
-
-clear.addEventListener("click", () => {
-    firstNumber = "ERROR";
-    secondNumber = "ERROR";
-    currentOperator = "ERROR";
-    isResult = true;
-    display.textContent = "";
-});
-
-erase.addEventListener("click", () => {
-    var currentDisplay = display.textContent;
-    if (currentDisplay !== "" && !isResult){
-        display.textContent = currentDisplay.slice(0, currentDisplay.length-1);
-    }
-});
-
-decimal.addEventListener("click", () => {
-    if (!display.textContent.includes(".")){
-        display.textContent += ".";
-    }
-});
-
+}
